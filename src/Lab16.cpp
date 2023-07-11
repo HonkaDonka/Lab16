7a9d1e8deb0c091c797c9f0a0b6dff338bb7aed7
Vinson Huang <vinsonh789@gmail.com>
2023-07-10 19:26:30 -0500

    completed lab

---
+++ src/Lab16.cpp
@@ -0,0 +1,103 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "c:/Users/vinso/Documents/CTDIoT2023/Lab16/src/Lab16.ino"
+#include "oled-wing-adafruit.h"
+
+void setup();
+void loop();
+void setupBLE();
+#line 3 "c:/Users/vinso/Documents/CTDIoT2023/Lab16/src/Lab16.ino"
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+bool prevConnection = true;
+bool disconnect = false;
+const size_t UART_TX_BUF_SIZE = 20;
+
+void onDataReceived(const uint8_t *data, size_t len, const BlePeerDevice &peer, void *context);
+
+const BleUuid serviceUuid("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+
+const BleUuid rxUuid("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
+BleCharacteristic rxCharacteristic("rx", BleCharacteristicProperty::WRITE_WO_RSP, rxUuid, serviceUuid, onDataReceived, NULL);
+
+const BleUuid txUuid("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
+BleCharacteristic txCharacteristic("tx", BleCharacteristicProperty::NOTIFY, txUuid, serviceUuid);
+
+OledWingAdafruit display;
+
+void setup()
+{
+  display.setup();
+  display.clearDisplay();
+  display.display();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  RGB.control(true);
+
+  setupBLE();
+  BLE.on();
+}
+
+void loop()
+{
+  display.loop();
+  if (BLE.connected() && !prevConnection)
+  {
+    prevConnection = true;
+    RGB.color(0, 0, 255);
+    delay(5000);
+    uint8_t txBuf[UART_TX_BUF_SIZE];
+    String message = "Hello!\n";
+    message.toCharArray((char *)txBuf, message.length() + 1);
+    txCharacteristic.setValue(txBuf, message.length() + 1);
+  }
+  else if (!BLE.connected() && prevConnection)
+  {
+    prevConnection = false;
+    RGB.color(255, 255, 0);
+  }
+  if (disconnect)
+  {
+    BLE.disconnect();
+    BLE.off();
+    setupBLE();
+    BLE.on();
+    disconnect = false;
+  }
+}
+
+void onDataReceived(const uint8_t *data, size_t len, const BlePeerDevice &peer, void *context)
+{
+  for (size_t i = 0; i < len; i++)
+  {
+    char value = data[i];
+    if (value == *"1")
+    {
+      display.clearDisplay();
+      display.setCursor(0, 0);
+      display.println("Hello World!");
+      display.display();
+    }
+    else if (value == *"0")
+    {
+      display.clearDisplay();
+      display.display();
+      disconnect = true;
+    }
+  }
+}
+
+void setupBLE() {
+  BLE.addCharacteristic(txCharacteristic);
+  BLE.addCharacteristic(rxCharacteristic);
+
+  BleAdvertisingData data;
+  data.appendServiceUUID(serviceUuid);
+  BLE.advertise(&data);
+  BLE.setDeviceName("Vinson's Argon");
+}
